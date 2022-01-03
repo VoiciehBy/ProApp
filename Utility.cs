1c8b99bd2e1a64eea74185deab03f055666c8b8d
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-01-03 15:12:13 +0100

    Create Utility.cs

---
+++ Utility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Set of utility methods inside static Utility class.
+/// </summary>
+public static class Utility
+{
+	public static double randomDouble()
+    {
+        Random random = new Random();
+        return random.NextDouble();
+    }	
+}
