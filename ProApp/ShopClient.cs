8841fcbe9b1c0ff60d4bb5859b402118b5f051f8
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-01-09 22:28:11 +0100

    Added ShopClient class.

---
+++ ProApp/ShopClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProApp
+{
+    public class ShopClient
+    {
+        private string name;
+        private string surname;
+        private string id;
+        public ShopClient()
+        {
+            name = "None";
+            surname = "None";
+            id = "-1";
+        }
+        public ShopClient(string name = "None", string surname = "None", string id = "-1")
+        {
+            this.name = name;
+            this.surname = surname;
+            this.id = id;
+        }
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public string Id { get => id; set => id = value; }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
