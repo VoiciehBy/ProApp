fc0eead9048586a83ff679fd9f0629a7613c63f3
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2023-07-07 15:31:58 +0200

    Create DDL.sql

---
+++ ProApp/DDL.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE [proapptest];
+
+CREATE TABLE products
+( 
+	id decimal(3,0),
+	name VARCHAR(255) NOT NULL,
+    amount decimal(6,2) NOT NULL,
+	isCountable decimal(1,0) DEFAULT 1,
+	unit varchar(5) NOT NULL,
+	basePrice decimal(6,2) NOT NULL,
+	CONSTRAINT products_PK PRIMARY KEY(id)
+);
